e01b416fd9288699bf7d9aa9b928066a0e067fec
tarna <>
2021-04-10 02:37:41 +0000

    added permission to a new role to say command

--- src/commands/say.js
+++ src/commands/say.js
@@ -19,9 +19,10 @@ class SayCommand extends Command {
 
 	async exec(msg, args) {
 
-		if (!msg.member.roles.cache.has('784654682316079104')) return msg.reply('You cannot use this command!');
+		if (!msg.member.roles.cache.has('784654682316079104' || !msg.member.roles.cache.has('784632424364769290'))) return msg.reply('You cannot use this command!');
 
 		msg.guild.channels.cache.get(args.channel.id).send(args.text);
+		msg.channel.send('Message sent!');
 
 	}
 }
