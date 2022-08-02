b3fe69a84ce07c01a34a292d893b17d99ef45415
tarna <46247728+tarna@users.noreply.github.com>
2022-08-01 23:01:24 -0400

    say command perms

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -1,5 +1,6 @@
 import { ChatInputCommand, Command, RegisterBehavior } from '@sapphire/framework';
-import { MessageEmbed, TextChannel } from 'discord.js';
+import { GuildMember, MessageEmbed, TextChannel } from 'discord.js';
+import { config } from '../config';
 
 export class SayCommand extends Command {
     constructor(context: Command.Context, options: Command.Options) {
@@ -19,7 +20,12 @@ export class SayCommand extends Command {
         );
     }
 
-    chatInputRun(interaction: Command.ChatInputInteraction) {
+    async chatInputRun(interaction: Command.ChatInputInteraction) {
+        if(!interaction.guild) return;
+        const member = interaction.member as GuildMember;
+        const canUse = member.roles.cache.some(r=> config.modRoles.includes(r.name));
+        if(!canUse) return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+ 
         const channelId = interaction.options.get('channel')?.value as string;
         const channel = interaction.guild?.channels.cache.get(channelId) as TextChannel;
         const message = interaction.options.get('message')?.value as string;
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,12 @@
 export const config: BotConfig = {
     welcomeChannel: process.env.WELCOME_CHANNEL!,
+    modRoles: [
+        'untreated',
+        'cute',
+    ]
 }
 
 export interface BotConfig {
     welcomeChannel: string;
+    modRoles: string[];
 }
